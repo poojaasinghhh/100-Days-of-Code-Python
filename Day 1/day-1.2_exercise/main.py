5edb7be922524c2b3261e67add136e4c94b81471
poojaasinghhh <137191212+poojaasinghhh@users.noreply.github.com>
2023-07-03 12:17:50 +0530

    Add files via upload

---
+++ Day 1/day-1.2_exercise/main.py
@@ -0,0 +1,4 @@
+print("Day 1 - String manipulation ")
+print('String concatenation is done with "+" sign.')
+print('e.g. print("hello" + "world")')
+print("New lines are created with a backslash and n.")
